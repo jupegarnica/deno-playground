0475ccf61f41923c36c14b42c296ad63b5d3beb2
juan <jupegarnica@gmail.com>
2020-05-14 09:05:12 +0200

    server with oak

--- server.ts
+++ server.ts
@@ -1,13 +1,40 @@
-import { serve } from 'https://deno.land/std@v0.42.0/http/server.ts';
+import {
+  Application,
+  Router,
+} from "https://deno.land/x/oak/mod.ts";
+const PORT = Number(Deno.env.get("PORT")) || 8080;
 
-const PORT = Number(Deno.env.get('PORT')) || 8080;
+const app = new Application();
 
-const s = serve({ port: PORT });
+// Logger
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.headers.get("X-Response-Time");
+  console.log(
+    `${ctx.request.method} ${ctx.request.url} - ${rt}`,
+  );
+});
 
-console.log(`http://localhost:${PORT}/`);
+// Timing
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${ms}ms`);
+});
+const router = new Router();
+router.get("/ping", async (ctx) => {
+  const body = await ctx.request.body();
+  console.log(body?.value);
 
-for await (const req of s) {
-  const headers = new Headers();
-  headers.set('Content-Type', 'application/json');
-  req.respond({ body: JSON.stringify(req), headers });
-}
+  ctx.response.body = body;
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.use((ctx) => {
+  // ctx.throw(404);
+  ctx.response.status = 404;
+  ctx.response.body = 404;
+});
+await app.listen({ port: PORT });
